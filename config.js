e31c894f01314baf92dc261bfa646a2c47d8169f
guptashivani-repos <87570342+guptashivani-repos@users.noreply.github.com>
2026-08-17 11:59:11 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// After deploying the Apps Script backend, paste the /exec URL below.
-// Example: https://script.google.com/macros/s/AKfycb.../exec
-window.ARYA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxp_D2Lgu3MiTU2qVE-DbNOdwCHpuBvV-dwZVGx5_KcJ3wbat8JCHgOjFkRlcjmBNo/exec"
-};
